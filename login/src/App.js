87ccec7cee224246e45c09a1b14e8e10210e7439
OttovonLauenburg <xiaoxinyi1688@163.com>
2020-10-27 11:55:50 -0700

    project

---
+++ login/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './component/Form.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
